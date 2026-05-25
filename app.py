acc1e21a604f908cf433dfb4add1ea223a9f2099
saulo camarillo <sacs2810@gmail.com>
2026-05-25 12:05:03 -0600

    Creating the files for docker practice

---
+++ app.py
@@ -0,0 +1,17 @@
+
+---
+
+## app.py
+
+```python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+if __name__ == "__main__":
+    app.run()
+
